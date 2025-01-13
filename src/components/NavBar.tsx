4d5ca9706d9dfaabbb6f222e3b5557d587f5c624
jfdf <jorcornejo5888@gmail.com>
2025-01-13 15:21:31 -0500

    listo2

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -60,6 +60,7 @@ const Navbar: React.FC = () => {
       {isMenuOpen && (
         <div className="absolute top-20 right-0 w-full bg-[#33587d] text-white text-center py-4 md:hidden">
           {[{ name: "Proyectos", path: "/proyectos" },
+          { name: "Sobre mí", path: "/sobremi" },
           { name: "Cursos", path: "/cursos" },
           { name: "Servicios", path: "/servicios" },
           { name: "Contactos", path: "/contacto" },
